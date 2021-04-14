4001d27997e4ec31bdc45e85cb069d45c8d50dcb
Layne Johnson <layne@findum.com>
2021-04-14 11:37:40 -0400

    Card model and image view subclass

---
+++ Tufoli/Model/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Tufoli
+//
+//  Created by Layne Johnson on 4/14/21.
+//
+
+import Foundation
+
+class Card {
+    
+    var imageName = ""
+    var isMatched = false
+    var isFlipped = false
+    
+}
