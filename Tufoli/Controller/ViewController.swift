653612f2f6efe809f146baa73df5e12daba92d15
Layne Johnson <layne@findum.com>
2021-04-15 08:45:20 -0400

    Configure stub methods UICollectionView

--- Tufoli/Controller/ViewController.swift
+++ Tufoli/Controller/ViewController.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Set the view contoller as the object that is supplying the data to the collection + handle user events in collection view
+
+// The view controller should conform to both delegate and datasource protocols for the UI Collection View Element
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     let model = CardModel()
     var cardsArray = [Card]()
@@ -17,6 +23,36 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         cardsArray = model.getCards()
+    }
+    
+    // MARK: - Collection View Delegate Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // Stub methods. Number of items to display in a section; method returns integer.
+        
+        // Return number of cards
+        return cardsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // Index path parameter gives cell coordinates (section, row) to reference cell; target cell.
+        
+        // Method asks the datasource which cell to display for each cell (0-n).
+        
+        // Method returns an entire UICollectionViewCell.
+        
+        // Get a cell
+        // Use reuse identifier given to prototype cell ("CardCell")
+        // Pass in indexPath parameter
+        // dequeueReusableCell returns the cell to us, either creating a new one or recycling cells
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        // TODO: Configure cell
+        
+        // Return cell
+        return cell
     }
 
 
